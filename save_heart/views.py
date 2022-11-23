bed6d0355ca25126594afa9aa3ce7001d61bf652
Debjit Pal <debjitpal5040@gmail.com>
2022-11-23 17:40:07 +0530

    polishing

--- save_heart/views.py
+++ save_heart/views.py
@@ -1,30 +1,32 @@
-import imp
+"""views.py"""
 from django.shortcuts import render
-import pandas as pd
 import numpy as np
 from joblib import load
 
 clf = load("Notebooks/dt_model.joblib")
 def index(req):
+    """View function for home page of site."""
     return render(req,'index.html')
 
 def result(pred):
+    """View function for result page of site."""
     data = predict(pred)
     return render(pred,'result.html', {'data':data})
 
 def predict(req):
+    """View function for prediction page of site."""
     age = req.POST['age']
     sex = req.POST['sex']
-    cp = req.POST['cp']
+    cp_ = req.POST['cp']
     trestbps = req.POST['trestbps']
     chol = req.POST['chol']
     fbs = req.POST['fbs']
     thalach = req.POST['thalach']
     exang = req.POST['exang']
     thal = req.POST['thal']
-    X=[[age, sex, cp, trestbps, chol, fbs, thalach, exang, thal]]
-    X=np.array(X).reshape(1, 9)
-    X=np.array(X, dtype=float)
-    y_pred=clf.predict(X).tolist()
+    x_test=[[age, sex, cp_, trestbps, chol, fbs, thalach, exang, thal]]
+    x_test=np.array(x_test).reshape(1, 9)
+    x_test=np.array(x_test, dtype=float)
+    y_pred=clf.predict(x_test).tolist()
     ans=y_pred[0]
     return ans
